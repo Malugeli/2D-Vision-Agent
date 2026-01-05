b929039a5f0895749846811cc2e15a17d037fb08
Malugeli <alugeli@gmail.com>
2026-01-05 18:36:57 +0100

    Ein Faktor für die UI erstellt um dynamisch hin und her zu gehen

---
+++ faktor.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <array>
+#include <utility>
+
+struct Faktorisierung{
+    double x;
+    double y;
+};
+
+enum class UiTarget{
+    out,
+    in,
+    scrollbar,
+    searchbar,
+
+    COUNT // Sagt die Menge der Elemente an
+    };
+
+constexpr std::array<Faktorisierung, std::to_underlying(UiTarget::COUNT)> UI{{
+    {0.828515625, 0.4701388888888889}, //Das ist für "OUT"
+    {0.4609375, 0.3951388888888889}, //Das ist für "IN"
+    {0.9703125, 0.3548611111111111}, // Scrollbar
+    {0.781640625, 0.1909722222222222} // Searchbar
+}};
