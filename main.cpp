d88fbdcf9a4660a2abf36c78558dfca75af26f2f
Malugeli <alugeli@gmail.com>
2026-01-07 21:35:12 +0100

    Hat geklappt, brauche ich den Faktor eigentlich so?

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 
 void drag(POINT startcord, POINT targetcord);
 void click(POINT p);
-int normalize(int coordinates, int maxDimension);
+//int normalize(int coordinates, int maxDimension);
 void getAlbaz(int targetX, int maxWidth, int targetY, int maxHeight, cv::Mat albazklein, cv::Mat screenshotBGR);
 std::string foa = "Fallen of Albaz";
 INPUT input{};    
@@ -24,31 +24,31 @@ POINT goal{};
 struct ClientSide{// bisher sind wir nicht dynamisch. Wenn User bei Laufzeit die Resolution ändert ist ClientRect falsch.
     HWND game;
     RECT ClientRect;
-    int width;
-    int height;
-
+    int width = 3840;
+    int height = 2160;
+ 
     ClientSide() = default;
     ClientSide(HWND input) : game(input != NULL ? input : nullptr){
         GetClientRect(game, &ClientRect);
-        width = GetSystemMetrics(SM_CXSCREEN);
-        height = GetSystemMetrics(SM_CYSCREEN);
+        // width = GetSystemMetrics(SM_CXSCREEN); Daten stimmen nicht, alte Funktion wir hardcoden die zahlen fürs erste
+        // height = GetSystemMetrics(SM_CYSCREEN);
     }
     
     POINT convert_coordinates(POINT p){ // Konvertiert ClientToScreenPixel zu ScreenPixel und normalisiert für SendInput (ich empfehle die Funktion nur als Argument für SendInput zu nutzen da User Fenster ansonsten bewegen kann und Daten outdated werden)
         ClientToScreen(game, &p);
-        p.x = normalize(p.x, width);
-        p.y = normalize(p.y, height);        
+        return normalize(p);       
     }
 
     POINT get_UI_coordinates(UiTarget target){ // gib nur das UI ein und du erhältst SendInput Ready Koordinaten
         POINT p;
-        p.x = normalize(std::lround(ClientRect.right * UI[std::to_underlying(target)].x), width);
-        p.y = normalize(std::lround(ClientRect.bottom * UI[std::to_underlying(target)].y), height);
+        p.x = std::lround(ClientRect.right * UI[std::to_underlying(target)].x);
+        p.y = std::lround(ClientRect.bottom * UI[std::to_underlying(target)].y);
+        return convert_coordinates(p);
 }
 
-
-    
-    
+    POINT normalize(POINT p){
+    return {(p.x * 65535) / width, (p.y *65535) / height};
+}    
 };
 
 //am ende des Tages wirds wahrscheinlich mindestens 2 Klassen geben 1. die ClientSide von Game und Koordinatenberechnung und 2. Der Bot selbst.
@@ -56,6 +56,7 @@ int main()
 {
     SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
     HWND game = FindWindow(NULL, "masterduel");
+    ClientSide ygo(game);
     input.type = INPUT_MOUSE; //ist hässlich idk ob ich das hier so lasse
     inputk.type = INPUT_KEYBOARD; //same
     RECT rect;
@@ -63,7 +64,6 @@ int main()
     int height = rect.bottom - rect.top;
     int width = rect.right - rect.left;
     cv::Rect maher(rect.left, rect.top, width, height);
-    UI[std::to_underlying(UiTarget::out)].x;
 
     wil::com_ptr<ID3D11Device> device;
     wil::com_ptr<ID3D11DeviceContext> context;
@@ -136,18 +136,18 @@ int main()
             double maxVal;
             cv::Point p;
             cv::minMaxLoc(result, &minVal, &maxVal, NULL, &p);
-            
+
 
             if (maxVal > 0.8) {
-                // std::cout << "Gefunden! King Maher! ";
-                // start.x = normalize(p.x + rect.left + (albaz.cols / 2), desc.Width);
-                // start.y = normalize(((p.y + rect.top) + (albaz.rows / 2)), desc.Height);
-                // goal = get_cords(UiTarget::searchbar, crect.right, crect.bottom);
-                ClientToScreen(game, &goal);
-                goal.x = normalize(goal.x, desc.Width);
-                goal.y = normalize(goal.y, desc.Height);
-                //drag(start, goal);
-                click(goal);
+                // // std::cout << "Gefunden! King Maher! ";
+                // // start.x = normalize(p.x + rect.left + (albaz.cols / 2), desc.Width);
+                // // start.y = normalize(((p.y + rect.top) + (albaz.rows / 2)), desc.Height);
+                // // goal = get_cords(UiTarget::searchbar, crect.right, crect.bottom);
+                // ClientToScreen(game, &goal);
+                // goal.x = normalize(goal.x, desc.Width);
+                // goal.y = normalize(goal.y, desc.Height);
+                // //drag(start, goal);<
+                click(ygo.get_UI_coordinates(UiTarget::searchbar));
             }
         }
         context->Unmap(cpuframe.get(), 0);
@@ -156,89 +156,86 @@ int main()
 }
 
 
-int normalize(int coordinates, int maxDimension){
-    return (coordinates * 65535) / maxDimension;
-}
 
-void getAlbaz(int targetX, int maxWidth, int targetY, int maxHeight, cv::Mat albazklein, cv::Mat screenshotBGR){
-    //geh hin
-    input.mi.dx = normalize(targetX, maxWidth),
-    input.mi.dy = normalize(targetY, maxHeight),
-    input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
-    SendInput(1, &input, sizeof(input));
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+// void getAlbaz(int targetX, int maxWidth, int targetY, int maxHeight, cv::Mat albazklein, cv::Mat screenshotBGR){
+//     //geh hin
+//     input.mi.dx = normalize(targetX, maxWidth),
+//     input.mi.dy = normalize(targetY, maxHeight),
+//     input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+//     SendInput(1, &input, sizeof(input));
+//     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    //klick
-    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-    SendInput(1, &input, sizeof(input));
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+//     //klick
+//     input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+//     SendInput(1, &input, sizeof(input));
+//     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    //klick go
-    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-    SendInput(1, &input, sizeof(input));
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+//     //klick go
+//     input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+//     SendInput(1, &input, sizeof(input));
+//     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    //------------------------------------------------------------------------
+//     //------------------------------------------------------------------------
 
-    for(char c : foa){
-        //TASTATURBEGINN
-        BYTE virtualKey = VkKeyScan(c);
-        inputk.ki.wScan = MapVirtualKey(virtualKey, MAPVK_VK_TO_VSC);
+//     for(char c : foa){
+//         //TASTATURBEGINN
+//         BYTE virtualKey = VkKeyScan(c);
+//         inputk.ki.wScan = MapVirtualKey(virtualKey, MAPVK_VK_TO_VSC);
         
-        // WICHTIG FÜR GAMES: Wir nutzen Scan Codes, nicht Virtual Keys
-        inputk.ki.dwFlags = KEYEVENTF_SCANCODE; // Drücken (Key Down)
+//         // WICHTIG FÜR GAMES: Wir nutzen Scan Codes, nicht Virtual Keys
+//         inputk.ki.dwFlags = KEYEVENTF_SCANCODE; // Drücken (Key Down)
 
-        // --- KEY DOWN ---
-        SendInput(1, &inputk, sizeof(INPUT));
+//         // --- KEY DOWN ---
+//         SendInput(1, &inputk, sizeof(INPUT));
 
-        // Kurze Pause, damit das Spiel den "Press" registriert (1-2 Frames)
-        // Ohne Sleep ist es oft zu schnell für die Game-Engine
-        std::this_thread::sleep_for(std::chrono::milliseconds(50)); 
+//         // Kurze Pause, damit das Spiel den "Press" registriert (1-2 Frames)
+//         // Ohne Sleep ist es oft zu schnell für die Game-Engine
+//         std::this_thread::sleep_for(std::chrono::milliseconds(50)); 
 
-        // --- KEY UP ---
-        inputk.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP; // Loslassen
-        SendInput(1, &inputk, sizeof(INPUT));
+//         // --- KEY UP ---
+//         inputk.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP; // Loslassen
+//         SendInput(1, &inputk, sizeof(INPUT));
         
-   }
-   std::this_thread::sleep_for(std::chrono::milliseconds(50));
-   inputk.ki.wScan = MapVirtualKey(VK_RETURN, MAPVK_VK_TO_VSC);
-   inputk.ki.dwFlags = KEYEVENTF_SCANCODE;
-   SendInput(1, &inputk, sizeof(INPUT));
-   std::this_thread::sleep_for(std::chrono::milliseconds(50));
-   inputk.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP; // Loslassen
-   SendInput(1, &inputk, sizeof(INPUT));
-   std::this_thread::sleep_for(std::chrono::milliseconds(50));
+//    }
+//    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+//    inputk.ki.wScan = MapVirtualKey(VK_RETURN, MAPVK_VK_TO_VSC);
+//    inputk.ki.dwFlags = KEYEVENTF_SCANCODE;
+//    SendInput(1, &inputk, sizeof(INPUT));
+//    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+//    inputk.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP; // Loslassen
+//    SendInput(1, &inputk, sizeof(INPUT));
+//    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
 
-   input.mi.dx = input.mi.dx * 1.23;
-   input.mi.dy = input.mi.dy * 1.5;
-   input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
-   SendInput(1, &input, sizeof(input));
+//    input.mi.dx = input.mi.dx * 1.23;
+//    input.mi.dy = input.mi.dy * 1.5;
+//    input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+//    SendInput(1, &input, sizeof(input));
 
-   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-   input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-   SendInput(1, &input, sizeof(input));
+//    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+//    SendInput(1, &input, sizeof(input));
 
-   std::this_thread::sleep_for(std::chrono::milliseconds(50));
+//    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-   input.mi.dy = input.mi.dy * 1.3;
-   input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
-   SendInput(1, &input, sizeof(input));
+//    input.mi.dy = input.mi.dy * 1.3;
+//    input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+//    SendInput(1, &input, sizeof(input));
 
-   std::this_thread::sleep_for(std::chrono::milliseconds(50));
+//    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-   input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-   SendInput(1, &input, sizeof(input));
+//    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+//    SendInput(1, &input, sizeof(input));
 
-   cv::Mat result;
-   cv::matchTemplate(screenshotBGR, albazklein, result, cv::TM_CCOEFF_NORMED);
+//    cv::Mat result;
+//    cv::matchTemplate(screenshotBGR, albazklein, result, cv::TM_CCOEFF_NORMED);
    
-   double minVal;
-   double maxVal;
-   cv::Point p;
-   cv::minMaxLoc(result, &minVal, &maxVal, NULL, &p);
-}
+//    double minVal;
+//    double maxVal;
+//    cv::Point p;
+//    cv::minMaxLoc(result, &minVal, &maxVal, NULL, &p);
+// }
 
 void drag(POINT startcord, POINT targetcord){
     input.mi.dx = startcord.x,
